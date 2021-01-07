199094b9965d447e1c6743404584acddc02a55e4
Tim Nash <site@timnash.co.uk>
2021-01-07 17:08:02 +0000

    All non Vendor files should Conform to WordPress Coding Standard

--- src/PeterHegman/Dumper.php
+++ src/PeterHegman/Dumper.php
@@ -4,17 +4,16 @@ namespace PeterHegman;
 
 use Symfony\Component\VarDumper\Cloner\VarCloner;
 
-class Dumper
-{
-    /**
-     * Dump a value with elegance.
-     *
-     * @param  mixed  $value
-     * @return void
-     */
-    public function dump($value)
-    {
-        $dumper = new HtmlDumper;
-        $dumper->dump((new VarCloner)->cloneVar($value));
-    }
+class Dumper {
+
+	/**
+	 * Dump a value with elegance.
+	 *
+	 * @param  mixed  $value
+	 * @return void
+	 */
+	public function dump( $value ) {
+		$dumper = new HtmlDumper();
+		$dumper->dump( ( new VarCloner() )->cloneVar( $value ) );
+	}
 }
--- wp-debug-helpers.php
+++ wp-debug-helpers.php
@@ -22,89 +22,90 @@ if ( ! defined( 'WPINC' ) ) {
 require __DIR__ . '/vendor/autoload.php';
 
 // If we haven't loaded this plugin from Composer we need to add our own autoloader
-if (!class_exists('PeterHegman\Dumper')) {
-    // Get a reference to our PSR-4 Autoloader function that we can use to add our
-    $autoloader = require_once('autoload.php');
+if ( ! class_exists( 'PeterHegman\Dumper' ) ) {
+	// Get a reference to our PSR-4 Autoloader function that we can use to add our
+	$autoloader = require_once 'autoload.php';
 
-    // Use the autoload function to setup our class mapping
-			$autoloader('PeterHegman\\', __DIR__ . '/src/PeterHegman/');
+	// Use the autoload function to setup our class mapping
+			$autoloader( 'PeterHegman\\', __DIR__ . '/src/PeterHegman/' );
 }
 
 /**
-* Disable Wordpress emoji's as they mess with the var-dumper
+* Disable WordPress emoji's as they mess with the var-dumper
 */
-remove_action('wp_head', 'print_emoji_detection_script', 7);
-remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
 
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
-if (! function_exists('dd')) {
-    /**
-     * Dump the passed variables and end the script.
-     *
-     * @param  mixed
-     * @return void
-     */
-    function dd()
-    {
-        array_map(function ($x) {
-            (new PeterHegman\Dumper)->dump($x);
-        }, func_get_args());
+if ( ! function_exists( 'dd' ) ) {
+	/**
+	 * Dump the passed variables and end the script.
+	 *
+	 * @param  mixed
+	 * @return void
+	 */
+	function dd() {
+		array_map(
+			function ( $x ) {
+				( new PeterHegman\Dumper() )->dump( $x );
+			},
+			func_get_args()
+		);
 
-        die(1);
-    }
+		die( 1 );
+	}
 }
 
-if (!function_exists('dump')) {
-    /**
-     * Dump the passed variables
-     *
-     * @param  mixed
-     * @return void
-     */
-    function dump()
-    {
-			echo
-        array_map(function ($x) {
-            (new PeterHegman\Dumper)->dump($x);
-        }, func_get_args());
-    }
+if ( ! function_exists( 'dump' ) ) {
+	/**
+	 * Dump the passed variables
+	 *
+	 * @param  mixed
+	 * @return void
+	 */
+	function dump() {
+			echo array_map(
+				function ( $x ) {
+					( new PeterHegman\Dumper() )->dump( $x );
+				},
+				func_get_args()
+			);
+	}
 }
 
-if (!function_exists('dc')) {
+if ( ! function_exists( 'dc' ) ) {
 	/**
 	 * Dump the passed variables to Console
 	 *
 	 * @param  mixed
 	 * @return void
 	 */
-	function dc($x, $s=true)
-	{
-		$o = 'console.log('.json_encode($x, JSON_HEX_TAG).');';
-		if($s) {
-			$o = '<script>'.$o.'</script>';
+	function dc( $x, $s = true ) {
+		$o = 'console.log(' . wp_json_encode( $x, JSON_HEX_TAG ) . ');';
+		if ( $s ) {
+			$o = '<script>' . $o . '</script>';
 		}
 		echo $o;
 	}
 }
 
-if (!function_exists('backtrace')) {
+if ( ! function_exists( 'backtrace' ) ) {
 	/**
 	 * Print out the current backtrace
 	 *
 	 * @param  mixed
 	 * @return void
 	 */
-	function backtrace()
-	{
+	function backtrace() {
 		ob_start();
 		debug_print_backtrace();
 		$t = ob_get_contents();
 		ob_end_clean();
 		//Remove the first method, because its this one!
-		$t = preg_replace ('/^#0\s+' . __FUNCTION__ . "[^\n]*\n/", '', $t, 1);
+		$t = preg_replace( '/^#0\s+' . __FUNCTION__ . "[^\n]*\n/", '', $t, 1 );
 		//very hacky should have its own formatter
-		(new PeterHegman\Dumper)->dump($t);
+		( new PeterHegman\Dumper() )->dump( $t );
 	}
 }
